4efbd629a2fb522f4e40488928fbb3463f2de3c7
fledit-sh <44525727+fledit-sh@users.noreply.github.com>
2025-06-30 07:53:42 +0200

    Add dynamic version retrieval [version:patch]

--- lambda_explorer/tools/__init__.py
+++ lambda_explorer/tools/__init__.py
@@ -21,7 +21,8 @@ import verboselogs
 __author__ = "Noel Ernsting Luz"
 __copyright__ = "Copyright (C) 2022 Noel Ernsting Luz"
 __license__ = "Public Domain"
-__version__ = "1.0"
+
+from ..__version__ import __version__
 
 # -----------------------------------------------------------------------------
 # GLOBALS
